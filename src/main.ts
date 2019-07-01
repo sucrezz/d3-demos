a7f6cd01e7a946670198c052c666288c8ff609a4
sunze <201189796@qq.com>
2019-07-01 23:08:07 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./assets/common/css/common.css";
+import Element from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+Vue.config.productionTip = false;
+
+//使用element组建库
+Vue.use(Element);
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="home">
+    <header class="flex">
+      <div class="img">
+        <img alt="Vue logo" src="../assets/logo.png" />
+      </div>
+      <div class="word">
+        D3-DEMOS
+      </div>
+    </header>
+    <section class="flex content-box">
+      <nav :class="isCollapse ? 'unflod' : 'flod'">
+        <i :class="iconFold" @click.stop="toggleIconFold"></i>
+        <el-menu
+          default-active="1-4-1"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          :collapse="isCollapse"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span slot="title">导航一</span>
+            </template>
+            <el-menu-item-group>
+              <span slot="title">分组一</span>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <span slot="title">选项4</span>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <i class="el-icon-document"></i>
+            <span slot="title">导航三</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导航四</span>
+          </el-menu-item>
+        </el-menu>
+      </nav>
+      <section>
+        <router-view></router-view>
+      </section>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Home extends Vue {
+  iconFold: string = "el-icon-s-unfold";
+  isCollapse: boolean = true;
+
+  toggleIconFold() {
+    this.iconFold =
+      this.iconFold == "el-icon-s-unfold"
+        ? "el-icon-s-fold"
+        : "el-icon-s-unfold";
+    this.isCollapse = !this.isCollapse;
+  }
+  handleOpen(key: any, keyPath: any) {
+    console.log(key, keyPath);
+  }
+  handleClose(key: any, keyPath: any) {
+    console.log(key, keyPath);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+}
+header {
+  height: 6rem;
+  line-height: 6rem;
+  box-shadow: 0 2px 3px #ccc;
+  .img {
+    width: 6.5rem;
+    height: 6rem;
+    position: relative;
+    img {
+      display: block;
+      width: 3rem;
+      height: 3rem;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+  }
+  .word {
+    flex: 1;
+    font-size: 2rem;
+    font-weight: 600;
+  }
+}
+.content-box {
+  height: calc(100% - 6rem);
+  overflow-y: auto;
+  overflow-x: hidden;
+  align-items: flex-start;
+  nav {
+    width: 6.5rem;
+    height: 100%;
+    background: #545c64;
+    position: relative;
+    .el-menu--collapse {
+      width: 100%;
+      box-sizing: border-box;
+    }
+    .el-menu-vertical-demo:not(.el-menu--collapse) {
+      width: 20rem;
+    }
+    &.unflod {
+      width: 6.5rem;
+    }
+    &.flod {
+      width: 20rem;
+    }
+    & > i {
+      position: absolute;
+      top: 1rem;
+      right: -3rem;
+      display: block;
+      width: 3rem;
+      height: 3rem;
+      font-size: 3rem;
+      cursor: pointer;
+    }
+  }
+  & > section {
+    flex: 1;
+    box-sizing: border-box;
+    padding-left: 3rem;
+  }
+}
+</style>
